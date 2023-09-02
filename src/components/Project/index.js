d062e9a0926f051b3836d0875368a010e1322c1f
Aarthi20 <aarthi2000cse@gmail.come>
2023-09-02 07:37:56 +0000

    projectshowcase

---
+++ src/components/Project/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const Project = props => {
+  const {projectDetails} = props
+  const {name, imageUrl} = projectDetails
+
+  return (
+    <li className="project-list">
+      <img src={imageUrl} alt={name} className="image" />
+      <p className="name">{name}</p>
+    </li>
+  )
+}
+export default Project
